9bd3e00e30b0448b3424e9374f0f4113482f6a50
silktreesolutions <joel.r.julian0408@gmail.com>
2019-05-20 13:33:01 +0530

    Adding db.py

---
+++ flaskr/db.py
@@ -0,0 +1,35 @@
+import click
+
+from flask import current_app, g
+from flask.cli import with_appcontext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+def get_db():
+    if 'db' not in g:
+        engine = create_engine('postgresql://postgres:admin@localhost:5432/books')
+        g.db = scoped_session(sessionmaker(bind=engine))
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.execute(f.read().decode('utf8'))
+    db.commit()
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
